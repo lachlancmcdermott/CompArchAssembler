2dab172edb7df14713a5d222da7a7cdb0d0a30f4
Lachlan McDermott <lachlancmcd@gmail.com>
2022-06-23 12:35:52 -0700

    SET added

---
+++ Assembler/FlowInstructions/SET.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assembler.FlowInstructions
+{
+    [Opcode(0x40)]
+    public class SET : Instruction
+    {
+        public override string Pattern => @"(SET) R([012][\d]|3[01]|\d) R([012][\d]|3[01]|\d) R([012][\d]|3[01]|\d)";
+
+        //different regex pattern
+
+        private FlowLayout flowLayout = new FlowLayout();
+        protected override ILayout layout => flowLayout;
+
+        public SET(byte[] data) : base(data)
+        {
+        }
+
+        public SET() : base()
+        {
+        }
+    }
---
+++ Assembler/Layouts/FlowLayout.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Assembler.Layouts
+{
+    public class FlowLayout : ILayout
+    {
+        Dictionary<string, byte> StringToOpCode = new Dictionary<string, byte>
+        {
+            ["SET"] = 0x40,
+        };
+
+        public byte[] Parse(Match match)
+        {
+            byte[] data = new byte[4];
+
+            //modify parse to work with flow instructions
+
+            return data;
+        }
+    }
+}
